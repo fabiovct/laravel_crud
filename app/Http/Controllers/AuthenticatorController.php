7a05c7fca03957b1f74f12c90cfc0fe0b5857902
fabiovct <vct.fabio.vct@gmail.com>
2021-03-13 08:24:49 -0300

    login

---
+++ app/Http/Controllers/AuthenticatorController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use DateTime;
+use Illuminate\Support\Facades\Auth;
+use \Firebase\JWT\JWT;
+use PDO;
+
+//use Tymon\JWTAuth\JWT as JWTAuthJWT;
+
+//use Validator;
+
+class AuthenticatorController extends Controller
+{
+
+    public function login(Request $request) {
+
+        $login = User::where('email','=',$request->email)->where('password','=',md5($request->password));
+        if($login->count() == 1){
+            $dados_user = User::where('email','=',$request->email)->where('password','=',md5($request->password))->first();
+            $time_start  = strtotime(now());
+
+            $key = env('SECRET_KEY');
+
+            $payload = array(
+                "login"=> $request->email,
+                "id_user"=>$dados_user->id,
+                //"iss" => "http://example.org",
+                //"aud" => "http://example.com",
+                "iat" => $time_start,
+                //"nbf" => $time_end
+            );
+
+            $jwt = JWT::encode($payload, $key);
+            //$decoded = JWT::decode($jwt, $key, array('HS256'));
+            //return response()->json($decoded);
+
+            return response()->json($jwt);
+        } else {
+            return 0;
+        }
+    }
+
+    // public function validateToken(Request $request){
+    //     $key = env('SECRET_KEY');
+    //     $time_now = strtotime(now());
+
+    //     try {
+    //         $decoded = JWT::decode($request->bearerToken(), $key, array('HS256'));
+    //     } catch (\Throwable $th) {
+    //         return redirect('error');
+    //     }
+
+    //     if( $decoded->iat+3600 < $time_now  ){
+    //         return redirect('error');
+    //     }
+
+    //     $payload = array(
+    //         "login"=> $decoded->login,
+    //         "id_user"=>$decoded->id_user,
+    //         //"iss" => "http://example.org",
+    //         //"aud" => "http://example.com",
+    //         "iat" => $time_now
+    //         //"nbf" => $time_end
+    //     );
+
+    //     $jwt = JWT::encode($payload, $key);
+
+    //     return response()->json(['jwt_status' => true,'token'=>$jwt]);
+    // }
+    /*public function drivers(){
+        print_r(PDO::getAvailableDrivers());
+        exit();
+    }*/
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('login', 'AuthenticatorController@login');
+// Route::post('validateToken', 'AuthenticatorController@validateToken');
+
+// Route::group(['middleware' => 'jwt'], function () {
+
+//     Route::group(['prefix' => 'products'], function () {
+//         Route::get('list-products', 'CustomerController@listProducts');
+//     });
+// });
